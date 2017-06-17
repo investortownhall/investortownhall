ffcf0ee6c297d91b2e8a7b8355d876d24f4ef210
Lenny Castaneda <lenny.castaneda@gmail.com>
2017-06-16 21:54:33 -0700

    16th commit

--- src/server.js
+++ src/server.js
@@ -2,23 +2,23 @@
  * Static HTTP Server
  *
  * Create a static file server instance to serve files
- * and folder in the './public' folder
+ * and folder in the './src' folder
  */
 
 // modules
-var static = require( 'node-static' ),
+var static = require('node-static'),
     port = 8080,
-    http = require( 'http' );
+    http = require('http');
 
 // config
-var file = new static.Server( './src', {
+var file = new static.Server('./src', {
     cache: 3600,
     gzip: true
-} );
+});
 
 // serve
-http.createServer( function ( request, response ) {
-    request.addListener( 'end', function () {
-        file.serve( request, response );
-    } ).resume();
-} ).listen( port );
+http.createServer(function(request, response) {
+    request.addListener('end', function() {
+        file.serve(request, response);
+    }).resume();
+}).listen(port);
